68f2ff05c74ea0d7534ea710d0e94565f7d8a49e
Mitchell Moore <mmoorejr24@gmail.com>
2020-11-23 15:17:00 -0800

    added basic styling to product cards.

--- src/components/pages/productspage/product.jsx
+++ src/components/pages/productspage/product.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Product({id, name, imageUrl, price, tags, colors}) {
+export default function Product({name, imageUrl, price, tags, colors}) {
+  const [hover, setHover] = useState (false);
   return (
-    <div className="product" key={id}>
-      <img src={imageUrl} alt={name} />
-      <div className="name">{name}</div>
-      <div className="price">Price: {price}</div>
-      <div className="tags">
-        Tags:
-        {' '}
-        {tags.map (tag => <span className="tag" key={tag}>{tag}</span>)}
-      </div>
-      <div className="tags">
-        Available in:
-        {' '}
-        {colors.map (color => <span className="tag" key={color}>{color}</span>)}
+    <div
+      className={`product ${hover && 'hover'}`}
+      onMouseEnter={() => setHover (true)}
+      onMouseLeave={() => setHover (false)}
+    >
+      <div className="card">
+        <img src={imageUrl} alt={name} />
+        <div className="name">{name}</div>
+        <div className="price">Price: {price}</div>
+        <div className="tags">
+          Tags:
+          {' '}
+          {tags.map (tag => <span className="tag" key={tag}>{tag}</span>)}
+        </div>
+        <div className="tags">
+          Available in:
+          {' '}
+          {colors.map (color => (
+            <span className="tag" key={color}>{color}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
