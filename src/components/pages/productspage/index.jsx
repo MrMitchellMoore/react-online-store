1d732bbf209e2d635858cf6a51e3dacbb527fce9
Mitchell Moore <mmoorejr24@gmail.com>
2020-11-23 11:57:03 -0800

    added Product component and Products.json files

--- src/components/pages/productspage/index.jsx
+++ src/components/pages/productspage/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Seo from '../seo';
+import Product from './product';
+import products from '../../data/products.json';
 
 export default function ProductsPage () {
   return (
     <React.Fragment>
-      <Seo title="Our Products" />
-      <h1>Shit we sell!</h1>
+      <Seo title="Our Products are awesome" />
+      <h1>Welcome to our store!</h1>
+      {products.map (product => <Product {...product} />)}
     </React.Fragment>
   );
 }
